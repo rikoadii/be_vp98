7b574c88ef7eed5f60dc9f973015a4d0f1c8a334
ADAM <2201020027@universitasbumigora.ac.id>
2025-08-18 17:43:20 +0800

    Refactor .htaccess and index.php for improved asset handling and path corrections

--- index.php
+++ index.php
@@ -5,16 +5,13 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/bootstrap/app.php')
+(require_once __DIR__ . '/../bootstrap/app.php')
     ->handleRequest(Request::capture());
-
-// /home/u967345369/domains/victoryproduction98.com/public_html/admin/index.php
-require __DIR__ . '/public/index.php';
